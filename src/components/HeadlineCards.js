633b6b9f4441f52dc64901606e5f4dceedbadb18
imksmv <98290303+imksmv@users.noreply.github.com>
2022-10-14 22:58:06 +0100

    HeadlineCards added

---
+++ src/components/HeadlineCards.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const HeadlineCards = () => {
+    return (
+        <section className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+            {/* Card */}
+            <div className="rounded-xl relative">
+                {/* Overlay */}
+                <div className="absolute h-full w-full bg-black/40 rounded-xl text-white flex flex-col justify-center items-start">
+                    <h3 className="font-bold px-2 text-2xl">BOGO's Out</h3>
+                    <p className="px-2">Through 8/26</p>
+                    <button className="border-white bg-white text-black m-2">
+                        Order Now
+                    </button>
+                </div>
+                <img
+                    className="max-h-[160px] md:min-h-[160px] w-full object-cover rounded-xl"
+                    src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="Wrap"
+                />
+            </div>
+            {/* Card */}
+            <div className="rounded-xl relative">
+                {/* Overlay */}
+                <div className="absolute h-full w-full bg-black/40 rounded-xl text-white flex flex-col justify-center items-start">
+                    <h3 className="font-bold px-2 text-2xl">New Restaurants</h3>
+                    <p className="px-2">Added Daily</p>
+                    <button className="border-white bg-white text-black m-2">
+                        Order Now
+                    </button>
+                </div>
+                <img
+                    className="max-h-[160px] md:min-h-[160px] w-full object-cover rounded-xl"
+                    src="https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="Steak"
+                />
+            </div>
+            {/* Card */}
+            <div className="rounded-xl relative">
+                {/* Overlay */}
+                <div className="absolute h-full w-full bg-black/40 rounded-xl text-white flex flex-col justify-center items-start">
+                    <h3 className="font-bold px-2 text-2xl">Fast Delivery</h3>
+                    <p className="px-2">24/7</p>
+                    <button className="border-white bg-white text-black m-2">
+                        Order Now
+                    </button>
+                </div>
+                <img
+                    className="max-h-[160px] md:min-h-[160px] w-full object-cover rounded-xl"
+                    src="https://images.pexels.com/photos/410648/pexels-photo-410648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="Wrap"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default HeadlineCards;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,11 +6,11 @@ const Hero = () => {
             <div className="relative">
                 {/* Overlay */}
                 <div className="absolute bg-black/40 w-full h-full text-gray-200 flex flex-col justify-center">
-                    <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+                    <h2 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                         The <span className="text-teal-400">Best</span>
                         <br />
                         <span className="text-teal-400">Foods</span> Delivered
-                    </h1>
+                    </h2>
                 </div>
                 <img
                     className="w-full max-h-[500px] object-cover"
